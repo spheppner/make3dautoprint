adcc9f80d58361ac852190f0b8a3dacb3182b08c
Simon Heppner <hasibandi@gmx.at>
2020-08-16 13:35:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 plugin_identifier = "make3dautoprint"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_make3dautoprint"
+plugin_package = "make3dautoprint"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
