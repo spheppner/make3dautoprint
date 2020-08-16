511f5b541de48f463f1e881e9f7ff6271f18d003
Simon Heppner <hasibandi@gmx.at>
2020-08-16 15:41:12 +0200

    Update __init__.py

--- make3dautoprint/__init__.py
+++ make3dautoprint/__init__.py
@@ -191,6 +191,7 @@ class Make3dAutoPrintPlugin(octoprint.plugin.SettingsPlugin,
         self._settings.set(["cp_queue"], json.dumps(queue))
         self._settings.save()
         self._logger.info("Automatic Add worked!")
+        return flask.jsonify(queue=queue)
     
     @octoprint.plugin.BlueprintPlugin.route("/removequeue", methods=["DELETE"])
     @restricted_access
